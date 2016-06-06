963f100e7c39e7bc853b5e1debc6c30a44e3b3b6
xxzhwx <xxzhwx@unknown.com>
2016-06-06 21:02:31 +0800

    add destroy function to Extension.

--- src/main/java/com/xxzhwx/core/extension/Extension.java
+++ src/main/java/com/xxzhwx/core/extension/Extension.java
@@ -6,4 +6,5 @@ public interface Extension {
     }
 
     boolean initialize();
+    boolean destroy();
 }
--- src/main/java/com/xxzhwx/core/extension/ExtensionManager.java
+++ src/main/java/com/xxzhwx/core/extension/ExtensionManager.java
@@ -1,13 +1,12 @@
 package com.xxzhwx.core.extension;
 
-import gnu.trove.iterator.TShortObjectIterator;
-import gnu.trove.map.TShortObjectMap;
-import gnu.trove.map.hash.TShortObjectHashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class ExtensionManager {
     private static final ExtensionManager I = new ExtensionManager();
 
-    private final TShortObjectMap<Extension> extensionMap = new TShortObjectHashMap<>();
+    private List<Ext> extList = new ArrayList<>();
 
     private ExtensionManager() {}
 
@@ -23,22 +22,51 @@ public final class ExtensionManager {
         }
 
         short extensionId = ann.id();
-        if (extensionMap.containsKey(extensionId)) {
+        if (this.contains(extensionId)) {
             throw new RuntimeException("Duplicated extension: " + extensionId);
         }
 
-        extensionMap.put(extensionId, extension);
+        extList.add(new Ext(extensionId, extension));
     }
 
     public boolean initialize() {
-        TShortObjectIterator<Extension> it = extensionMap.iterator();
-        while (it.hasNext()) {
-            Extension extension = it.value();
+        extList.sort((o1, o2) -> o1.id - o2.id);
+        for (Ext ext : extList) {
+            Extension extension = ext.ext;
             if (!extension.initialize()) {
                 System.out.println("Fail to initialize extension: " + extension.getName());
                 return false;
             }
         }
         return true;
+    }
+
+    public void destroy() {
+        for (int i = extList.size() - 1; i >= 0; --i) {
+            Ext ext = extList.get(i);
+            Extension extension = ext.ext;
+            if (!extension.destroy()) {
+                System.out.println("Fail to destroy extension: " + extension.getName());
+            }
+        }
+    }
+
+    private boolean contains(short extensionId) {
+        for (Ext ext : extList) {
+            if (ext.id == extensionId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static class Ext {
+        public final int id;
+        public final Extension ext;
+
+        public Ext(int id, Extension ext) {
+            this.id = id;
+            this.ext = ext;
+        }
     }
 }
