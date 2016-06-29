7378f5e1d254d85424b36af3c0b3b97a26f9a40f
xxzhwx <xxzhwx@unknown.com>
2016-06-29 20:55:18 +0800

    Support executing tasks in the main loop(Command Runnable).

--- src/main/java/com/xxzhwx/core/runs/AbstractCmdRunnable.java
+++ src/main/java/com/xxzhwx/core/runs/AbstractCmdRunnable.java
@@ -6,14 +6,28 @@ import com.xxzhwx.core.queue.BaseQueue;
 import java.util.concurrent.TimeUnit;
 
 public abstract class AbstractCmdRunnable implements Runnable {
+    private BaseQueue<Runnable> taskQueue;
     private BaseQueue<Cmd> cmdQueue;
 
     public AbstractCmdRunnable(BaseQueue<Cmd> cmdQueue) {
+        this.taskQueue = new BaseQueue<>();
         this.cmdQueue = cmdQueue;
+    }
+
+    public void submitTask(Runnable task) {
+        taskQueue.put(task);
     }
 
     public void run() {
         for (;;) {
+            Runnable task;
+            while (taskQueue.size() > 0) {
+                task = taskQueue.poll();
+                if (task != null) {
+                    task.run();
+                }
+            }
+
             Cmd cmd;
             while (cmdQueue.size() > 0) {
                 cmd = cmdQueue.poll();
